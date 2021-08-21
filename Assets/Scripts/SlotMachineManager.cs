fdbcf47f93e279495834f2daebe481907e4d64e1
Arefnue <arifmertcankara@gmail.com>
2021-08-22 01:57:59 +0300

    feat: Stop at time added

--- Assets/Scripts/SlotMachineManager.cs
+++ Assets/Scripts/SlotMachineManager.cs
@@ -1,61 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotMachineManager : MonoBehaviour
 {
-    public List<SlotController> slotControllerList;
+    [Header("Machine Slots")]
+    [SerializeField] private List<SlotController> slotControllerList;
+    
+    
+    [Header("Settings")]
+    [SerializeField] private float minChooseTime = 2f;
+    [SerializeField] private float maxChooseTime = 5f;
 
     [SerializeField] private float slotMinDelayTime = 0.1f;
     [SerializeField] private float slotMaxDelayTime = 0.5f;
-
-    private bool _isSlowStop = true;
     
-    private void Update()
-    {
-#if UNITY_EDITOR
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            StartCoroutine(StartSlotsRoutine());
-        }
-        
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            StartCoroutine(StopSlotsRoutine());
-        }
-#endif
-    }
+    [Header("References")]
+    [SerializeField] private Button playButton;
 
+    private readonly bool _isSlowStop = true;
+    private bool _canPlaySlotMachine = true;
+
+    #region Routines
 
     private IEnumerator StartSlotsRoutine()
     {
+        SetPlayButton(false);
+
         foreach (var slotController in slotControllerList)
         {
             slotController.StartSpinning();
-            yield return new WaitForSeconds(Random.Range(slotMinDelayTime,slotMaxDelayTime));
+            yield return new WaitForSeconds(Random.Range(slotMinDelayTime, slotMaxDelayTime));
         }
-    } 
+
+        yield return new WaitForSeconds(Random.Range(minChooseTime, maxChooseTime));
+
+        StartCoroutine(StopSlotsRoutine());
+    }
+
     private IEnumerator StopSlotsRoutine()
     {
+        var waitFinishTime = 0f;
         for (var i = 0; i < slotControllerList.Count; i++)
         {
-           
             yield return new WaitForSeconds(Random.Range(slotMinDelayTime, slotMaxDelayTime));
-            var slotController = slotControllerList[i];
 
-            if (i==(slotControllerList.Count-1))
-            {
-                slotController.StopSpinning(ScoreCard.CardType.A,_isSlowStop ? SlotController.StopType.Slow : SlotController.StopType.Normal);
-            }
+            var slotController = slotControllerList[i];
+            if (i == slotControllerList.Count - 1)
+                slotController.StopSpinning(ScoreCard.CardType.A,
+                    _isSlowStop ? SlotController.StopType.Slow : SlotController.StopType.Normal);
             else
-            {
                 slotController.StopSpinning(ScoreCard.CardType.A);
-            }
-           
+
+            waitFinishTime += slotController.FinishDelayTime;
         }
+
+        yield return new WaitForSeconds(waitFinishTime);
+        SetPlayButton(true);
     }
+
+    #endregion
+
+    #region Methods
     
-    
-    
-    
+    public void OnPlaySlotMachine()
+    {
+        if (_canPlaySlotMachine) StartCoroutine(StartSlotsRoutine());
+    }
+
+    private void SetPlayButton(bool canPlay)
+    {
+        _canPlaySlotMachine = canPlay;
+        playButton.interactable = canPlay;
+    }
+
+    #endregion
 }
