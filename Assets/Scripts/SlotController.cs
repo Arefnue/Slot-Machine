f11914b224d4943f85fb21dd07f54156f37843df
Arefnue <arifmertcankara@gmail.com>
2021-08-21 17:31:47 +0300

    feat: Basic spinning added

--- Assets/Scripts/SlotController.cs
+++ Assets/Scripts/SlotController.cs
@@ -1,8 +1,71 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SlotController : MonoBehaviour
 { 
     public List<ScoreCard> scoreCardList;
+
+    [SerializeField] private float spinSpeed = 5f;
+    [SerializeField] private float bottomThreshold = -7.5f;
+    [SerializeField] private float topThreshold = 5f;
+    
+
+    private bool _canSpin=false;
+    private bool _isSpinning=false;
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlaySlot();
+        }
+#endif
+    }
+
+    public void PlaySlot()
+    {
+        if (!_isSpinning)
+        {
+            StartCoroutine(SlotTurnRoutine());
+        }
+       
+    }
+
+    private IEnumerator SlotTurnRoutine()
+    {
+        var waitFrame = new WaitForEndOfFrame();
+        var timer = 0f;
+
+        _isSpinning = true;
+        _canSpin = true;
         
+        while (_canSpin)
+        {
+            timer += Time.deltaTime;
+
+            foreach (var scoreCard in scoreCardList)
+            {
+                var cardLocalPos =scoreCard.transform.localPosition;
+                cardLocalPos.y -= Time.deltaTime * spinSpeed;
+
+                if (cardLocalPos.y<=bottomThreshold)
+                {
+                    cardLocalPos.y = topThreshold;
+                }
+                
+                scoreCard.transform.localPosition = cardLocalPos;
+                
+            }
+            
+            yield return waitFrame;
+        }
+        
+        
+    }
+    
+    
+    
 }
