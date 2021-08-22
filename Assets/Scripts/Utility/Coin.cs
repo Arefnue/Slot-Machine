1df43f2ba41a2c8e42c9f817eabcfa86b63c8744
Arefnue <arifmertcankara@gmail.com>
2021-08-22 15:55:21 +0300

    feat: Temporary coin particle added

---
+++ Assets/Scripts/Utility/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Utility
+{
+    public class Coin : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody myRigidbody;
+        
+        public void ExplodeCoin(float minForce,float maxForce, Vector3 explosionPos,float explosionRadius, float upwardModifier)
+        {
+            myRigidbody.AddExplosionForce(Random.Range(minForce,maxForce),explosionPos,explosionRadius,upwardModifier,ForceMode.VelocityChange);
+            Destroy(gameObject,5f);
+        }
+    }
+}
--- Assets/Scripts/Utility/CoinSpawner.cs
+++ Assets/Scripts/Utility/CoinSpawner.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using SlotMachine;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Utility
 {
     public class CoinSpawner : MonoBehaviour
     {
         [SerializeField] private List<ScoreCardCoinData> scoreCardCoinDataList;
+        [SerializeField] private Coin coinPrefab;
+        [SerializeField] private Transform coinSpawnTransform;
+        [SerializeField] private float minExplosionValue;
+        [SerializeField] private float maxExplosionValue;
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                SpawnCoins(10);
+            }
+            
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                SpawnCoins(50);
+            }
+        }
+
+        public void SpawnCoins(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var coin = Instantiate(coinPrefab, coinSpawnTransform);
+                coin.ExplodeCoin(minExplosionValue,maxExplosionValue,coinSpawnTransform.position + new Vector3(Random.Range(-2,2),-1,1),5f,10);
+            }
+        }
+        
         
     }
     
